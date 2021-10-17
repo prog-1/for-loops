6266ed4bb841438606c6c595ac8026fbdea383d2
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-17 13:46:22 +0300

    adding and changing power-of-2.go

---
+++ power-of-2/power-of-2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The program determines if a number is a power of 2.")
+	var n, i int
+	fmt.Print("Enter the number: ")
+	fmt.Scan(&n)
+	if n > 0 && (n&(n-1)) == 0 {
+		for z := 1; z != n; z *= 2 {
+			i++
+
+		}
+		fmt.Printf("2^%v", i)
+	} else {
+		fmt.Println(n, "is not a power of 2.")
+	}
+
+}
