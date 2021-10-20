ff1820195ec1c31a731304cb8d516ced319e1547
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-20 14:14:52 +0300

    adding and changing

---
+++ prime-number/prime-number.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Println("The program determines if an integer is prime.")
+	fmt.Print("Enter the number: ")
+	fmt.Scan(&n)
+	for i := 1; i != n+1; i++ {
+		if n%2 == 0 && n != 2 ||
+			n%3 == 0 && n != 3 ||
+			n%5 == 0 && n != 5 {
+			fmt.Printf("%v is not a prime number.", n)
+			break
+		} else {
+			fmt.Printf("%v is a prime number.", n)
+			break
+		}
+	}
+}
