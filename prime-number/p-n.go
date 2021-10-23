8ffd8a42af6e2a6746f31829f16ce766c6631899
Alina Aniscenko <alina065b@gmail.com>
2021-10-23 19:07:19 +0300

    prime number

--- prime-number/p-n.go
+++ prime-number/p-n.go
@@ -5,16 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("The program determines if an integer is prime.")
 	fmt.Println("Enter the number:")
-	var num int //prime numbers are always possitive, because are >1
-	fmt.Scan(&num)
+	var num int    //prime numbers are always possitive, because are >1
+	fmt.Scan(&num) // 3; 45; 7; 8
 	if num <= 1 {
 		fmt.Println("Number must be greater than 1.")
 	}
 	for i := 2; i < num; i++ {
 		if num%i == 0 {
-			fmt.Println(num, "is not a prime number")
+			fmt.Println(num, "is not a prime number") // 45; 8
 			return
 		}
 	}
-	fmt.Println(num, "is a prime number")
+	fmt.Println(num, "is a prime number") // 3; 7
 }
