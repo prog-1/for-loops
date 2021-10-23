f6a311144ddee273267f1d7ecdf43c45b91e9e98
unknown <valerijamax@gmail.com>
2021-10-23 17:44:52 +0300

    adding corrected power-of-2

--- power-of-2/main.go
+++ power-of-2/main.go
@@ -11,8 +11,13 @@ func main() {
 	fmt.Scan(&n)
 	var y uint
 	var z uint
-	if n <= 0 {
+	if n == 1 {
+		fmt.Println(n, "=2^0")
+		return
+	}
+	if n <= 0 || n%2 != 0 {
 		fmt.Println(n, "is not a power of 2")
+		return
 	}
 	for x := n; x != 1; x /= 2 {
 		y = x % 2
