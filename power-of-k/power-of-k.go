78d978ce37d97521f7fe4acdc23a24c5f0485c36
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-23 11:03:05 +0300

    changing, but last print still does NOT work...

--- power-of-k/power-of-k.go
+++ power-of-k/power-of-k.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 	fmt.Println("The program determines if a number n is a power of k.")
-	var n, k int
+	var k int
 	var i int = 0
+	var n float64
 	fmt.Print("Enter n and k: ")
 	fmt.Scan(&n, &k)
-	if n%k == 0 { // to broke this program just write 62 2, or similar numbers, 'cause i don't know what else i can write here.
-		for z := 1; z != n; z *= k {
-			i++
-			fmt.Println(i) // counter
+	if n/(math.Sqrt(n)) == (math.Sqrt(n)) {
+		for z := 1; z != int(n); z *= k {
+			i++ // counter
+
 		}
 		fmt.Printf("%v = %v^%v", n, k, i)
 	} else {
-		fmt.Printf("%v is not a power of %v.", n, k)
+		fmt.Printf("%v is not a power of %v.", n, k) // why this line does not work???
 	}
 }
