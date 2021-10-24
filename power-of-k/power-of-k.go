1b309610bf95f0522ba6d307869528be423fcaef
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-24 13:07:10 +0300

    finally done power-of-k.go

--- power-of-k/power-of-k.go
+++ power-of-k/power-of-k.go
@@ -2,23 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	fmt.Println("The program determines if a number n is a power of k.")
-	var k int
+	var k, n int
 	var i int = 0
-	var n float64
 	fmt.Print("Enter n and k: ")
 	fmt.Scan(&n, &k)
-	if n/(math.Sqrt(n)) == (math.Sqrt(n)) {
-		for z := 1; z != int(n); z *= k {
-			i++ // counter
-
+	var d = n
+	if n%k == 0 {
+		for ; n != 1; n /= k {
+			i++
 		}
-		fmt.Printf("%v = %v^%v", n, k, i)
-	} else {
-		fmt.Printf("%v is not a power of %v.", n, k) // why this line does not work???
+		fmt.Printf("%v = %v ^ %v", d, k, i)
+		return
 	}
+	fmt.Printf("%v is not a power of %v.", n, k)
 }
