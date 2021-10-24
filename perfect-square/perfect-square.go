4ad83802ba7fc0609eefb92e04bc9f023282d03e
Marija Z <marija.zunda@gmail.com>
2021-10-24 22:12:45 +0300

    editing perfect-square

--- perfect-square/perfect-square.go
+++ perfect-square/perfect-square.go
@@ -6,14 +6,14 @@ import (
 
 func main() {
 	fmt.Println("The program determines if an integer is a perfect square.")
-	fmt.Println("Enter the number:")
-	var n int
+	fmt.Println("Enter the number:") 
+	var n int //4; 11; 23; 25
 	fmt.Scanln(&n)
 	for i := 0; i*i <= n; i++ {
 		if i*i == n {
-			fmt.Println(n, "Is a perfect square", i, "^2=", n)
+			fmt.Println(n, "Is a perfect square", i, "^2=", n) //4; 25
 			return
 		}
 	}
-	fmt.Println("Is not a perfect quare", n)
+	fmt.Println("Is not a perfect square", n) //11; 23
 }
