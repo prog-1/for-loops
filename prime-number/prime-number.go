58dcaab8ae3b994e6aed9f4f020da0161fd66f4c
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-24 18:38:18 +0300

    changing...

--- prime-number/prime-number.go
+++ prime-number/prime-number.go
@@ -7,16 +7,12 @@ func main() {
 	fmt.Println("The program determines if an integer is prime.")
 	fmt.Print("Enter the number: ")
 	fmt.Scan(&n)
-	for i := 1; i != n+1; i++ {
-		if n%2 == 0 && n != 2 ||
-			n%3 == 0 && n != 3 ||
-			n%5 == 0 && n != 5 ||
-			n%7 == 0 && n != 7 {
+	d := n
+	for s := 2; s != d; s++ {
+		if n%s == 0 {
 			fmt.Printf("%v is not a prime number.", n)
-			break
-		} else {
-			fmt.Printf("%v is a prime number.", n)
-			break
+			return
 		}
 	}
+	fmt.Printf("%v is a prime number.", n)
 }
