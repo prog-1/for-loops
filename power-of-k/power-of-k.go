86daf5111a2e79a52198a4d867394023c865feba
Marija Z <marija.zunda@gmail.com>
2021-10-24 22:14:15 +0300

    editing power-of-k

--- power-of-k/power-of-k.go
+++ power-of-k/power-of-k.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	fmt.Println("The program determines if a number n is a power of k.")
 	fmt.Println("Enter n and k:")
-	var n, k int
+	var n, k int //64, 4; 8, 3; 81, 9; 3, 2
 	fmt.Scanln(&n, &k)
 	var a, b int
 	if n == 1 {
@@ -18,8 +18,13 @@ func main() {
 		fmt.Println(n, "Is not a power of", k)
 		return
 	}
-	if a == 0 {
-		fmt.Println(n, "=", k, "^", b)
-	} else {
-		fmt.Println(n, "Is not a power of", k)
+	for r := n; r != 1; r /= k {
+		a = r % k
+		b++
 	}
+	if a == 0 {
+		fmt.Println(n, "=", k, "^", b) //64, 4; 81, 9
+	} else {
+		fmt.Println(n, "Is not a power of", k) //8, 3; 3, 2
+	}
+}
