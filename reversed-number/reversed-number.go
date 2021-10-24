24c08324a2e5a4791fc3fbfa00b3f3c17791cfab
Marija Z <marija.zunda@gmail.com>
2021-10-24 22:15:44 +0300

    editing reversed-number

--- reversed-number/reversed-number.go
+++ reversed-number/reversed-number.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	fmt.Println("The program reverses the digits in the number.")
 	fmt.Println("Enter the number:")
-	var d uint
+	var d uint //0; 2; 346; 4587
 	fmt.Scanln(&d)
 	var e uint
 	for rnumber := d; rnumber != 0; rnumber /= 10 {
 		e = e*10 + rnumber%10
 	}
-	fmt.Println("The reversed number is", e)
+	fmt.Println("The reversed number is", e) //0; 2; 643; 7854
 }
