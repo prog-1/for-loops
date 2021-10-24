4fdb47a198f7489c21698a40843bcd61313b0767
Marija Z <marija.zunda@gmail.com>
2021-10-24 22:11:07 +0300

    editing even-digits-

--- even-digits-/even-digits.go
+++ even-digits-/even-digits.go
@@ -7,15 +7,15 @@ import (
 func main() {
 	fmt.Println("The program determines if all digits in the integer are even.")
 	fmt.Println("Enter the number:")
-	var a int
+	var a int //-28; 379; 2806; 0; 33
 	fmt.Scanln(&a)
 	var b int
 	for evendigits := a; evendigits != 0; evendigits /= 10 {
 		b = evendigits % 10
 		if b%2 != 0 {
-			fmt.Println("Not all digits in", n, "are even")
+			fmt.Println("Not all digits in", a, "are even") //379; 33
 			return
 		}
 	}
-	fmt.Println("All digits in", n, "are even")
+	fmt.Println("All digits in", a, "are even") //-28; 2806; 0
 }
