d41cc94ea2cc74c2f703cfbe6d4c893dadbf1f9d
Sp0ozy <imolchanov210@gmail.com>
2021-10-24 20:18:49 +0300

    power of 2 done

---
+++ power-of-2/power-of-2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The program determines if a number is a power of 2.")
+	fmt.Println("Enter the number:")
+	var n, k int
+	fmt.Scanln(&n)
+	if n == 0 {
+		fmt.Print("error: 0 isn't a power of any number")
+	} else {
+		var tmp = n
+		if n >= 2 {
+			for ; tmp != 1; tmp /= 2 {
+				if tmp%2 == 0 {
+					k++
+				} else {
+					break
+				}
+
+			}
+		}
+
+		if tmp == 1 {
+			fmt.Print(n, "=2^", k)
+		} else {
+			fmt.Println(n, "is not a power of 2")
+		}
+	}
+
+}
+
+// tested 16
+//16=2^4
+//tested 9
+//9 is not a power of 2
+//tested 1
+//1=2^0
+//tested 0.06
+//0.06 is not a power of 2
+//tested -8
+//-8 is not a power of 2
+//tested 0
+//error: 0 isn't a power of any number
