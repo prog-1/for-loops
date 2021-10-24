4fcf74d7e67be3f4b20fc030fdf32ba640a353b7
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-24 18:04:19 +0300

    changing n to int

--- perfect-square/perfect-square.go
+++ perfect-square/perfect-square.go
@@ -6,11 +6,11 @@ import (
 
 func main() {
 	fmt.Println("The program determines if an integer is a perfect square.")
-	var n float64
+	var n int
 	fmt.Print("Enter the number: ")
 	fmt.Scan(&n)
-	for i := 1; i < int(n); i++ {
-		if int(n)/i == i {
+	for i := 1; i < n; i++ {
+		if n/i == i {
 			fmt.Printf("%v is a perfect square. %v^2 = %v", n, i, n)
 			return
 		}
