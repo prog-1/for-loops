3367c419a88f22190ce017b20b8a97f6c082a31f
Alina Aniscenko <alina065b@gmail.com>
2021-10-25 10:05:04 +0300

    power of 2 (without Pow)

--- power-of-2/p-of-2.go
+++ power-of-2/p-of-2.go
@@ -1,21 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("The program determines if a number is a power of 2.")
 	fmt.Println("Enter the number:")
-	var num float64 // 8; 16; 34; 64; 6
+	var num int
 	fmt.Scan(&num)
-	var i float64
-	for i = 0; math.Pow(2, i) <= num; i++ {
-		if math.Pow(2, i) == num {
-			fmt.Println(num, "= 2 ^", i) // 8; 16; 64
-			return
-		}
+	var n int
+	var res int
+	for n = num; n > 0 && n%2 == 0; n /= 2 {
+		res++
 	}
-	fmt.Println(num, "is not a power of 2.") // 34; 6
+	if n != 1 {
+		fmt.Println(num, "is not a power of 2")
+	} else {
+		fmt.Println(num, "is a power of 2.", num, "= 2 ^", res)
+	}
 }
