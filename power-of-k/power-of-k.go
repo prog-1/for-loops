559b4a4dc2235696f92c6847db26fbbb061476c1
Sp0ozy <imolchanov210@gmail.com>
2021-10-25 11:42:17 +0300

    power of k redone with negative numbers

--- power-of-k/power-of-k.go
+++ power-of-k/power-of-k.go
@@ -10,34 +10,53 @@ func main() {
 	var n, k, a int
 	fmt.Scanln(&n, &k)
 	if n == 0 {
-		fmt.Print("error: 0 isn't a power of any number")
-	} else {
-		var tmp = n
-		if n >= k {
-			for ; tmp != 1; tmp /= k {
-				if tmp%k == 0 {
-					a++
-				} else {
-					break
-				}
-
-			}
+		if k == 0 {
+			fmt.Println("power is any number except 0")
+			return
+		} else {
+			fmt.Print(n, "is not a power of", k)
+			return
 		}
 
-		if tmp == 1 {
-			fmt.Print(n, "=", k, "^", a)
-		} else {
-			fmt.Println(n, "is not a power of", k)
+	}
+	var tmp = n
+	if n > 0 {
+
+		for ; tmp != 1; tmp /= k {
+			if tmp%k == 0 {
+				a++
+			} else {
+				break
+			}
+
+		}
+
+	} else {
+		for ; tmp != -1; tmp /= k {
+			if tmp%k == 0 {
+				a++
+			} else {
+				break
+			}
+
 		}
 	}
-
+	if tmp == 1 {
+		fmt.Print(n, "=", k, "^", a)
+	} else {
+		fmt.Println(n, "is not a power of", k)
+	}
 }
 
 // tested 16 4
 //16=4^2
-//tested 0 5
-//error: 0 isn't a power of any number
+//tested 0 0
+//power is any number except 0
 //tested 1
 //1=2^0
 //tested 5 3
 //5 is not a power of 3
+//tested -64 4
+//-64 is not a power of 4
+// tested -8 -2
+// -8=-2^3
