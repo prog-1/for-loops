50b1a8ec6fbb8ea96ed0518f3e4acb9cb75d608d
Alina Aniscenko <alina065b@gmail.com>
2021-10-25 11:08:42 +0300

    e-d.go

--- even-digits/e-d.go
+++ even-digits/e-d.go
@@ -5,13 +5,37 @@ import "fmt"
 func main() {
 	fmt.Println("The program determines if all digits in the integer are even.")
 	fmt.Println("Enter the number:")
-	var x int // 456; 21; -5321; 2286; 24
+	var x int
 	fmt.Scan(&x)
 	for ed := x; ed != 0; ed /= 10 {
 		if (ed%10)%2 != 0 {
-			fmt.Println("Not digits in", x, "are even.") // 456; 21; -5321
+			fmt.Println("Not digits in", x, "are even.")
 			return
 		}
 	}
-	fmt.Println("All digits in", x, "are even.") // 2286; 24
+	fmt.Println("All digits in", x, "are even.")
 }
+
+// Test (1)
+// Input: 0
+// Output: All digits in 0 are even.
+
+// Test (2)
+// Input: 1
+// Output: Not digits in 1 are even.
+
+// Test (3)
+// Input: 34
+// Output: Not digits in 34 are even.
+
+// Test (4)
+// Input: 468
+// Output: All digits in 468 are even.
+
+// Test (5)
+// Input: -342
+// Output: Not digits in -342 are even.
+
+// Test (6)
+// Input: -682
+// Output: All digits in -682 are even.
