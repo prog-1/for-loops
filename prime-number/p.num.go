6f1f566ccecba21cdd5acf3ed1ba6f1779aecd77
Daniil Putrans <danikputranss@gmail.com>
2021-10-27 22:37:20 +0300

    loops

--- prime-number/p.num.go
+++ prime-number/p.num.go
@@ -7,6 +7,10 @@ func main() {
 	fmt.Println("Enter the number:")
 	var n int
 	fmt.Scan(&n)
+	if n<2{
+		fmt.Println(n, "is not a prime number")
+		return
+	}
 	var i int = 2
 	for i = 2; i < n; i++ {
 		if n%i == 0 {
@@ -18,5 +22,21 @@ func main() {
 }
 
 // Test:
-// Input: 2, 4, 199, 7, 16
-// Output: 2 is a prime number, 4 is not a prime number, 199 is a prime number, 7 is a prime number, 16 is not a prime number
+// Input: 1
+// Output: 1 is not a prime number
+
+// Test:
+// Input: 0
+// Output: 0 is not a prime number
+
+// Test:
+// Input: 2
+// Output: 2 is a prime number
+
+// Test:
+// Input: 56
+// Output: 56 is not a prime number
+
+// Test:
+// Input: 7
+// Output: 7 is a prime number
